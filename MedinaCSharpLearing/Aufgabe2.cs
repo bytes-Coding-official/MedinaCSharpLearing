24c509c05813094c90503a8337bdab6a70f43e04
Carina Sophie Schoppe <schoppe@bytes-coding.com>
2024-05-31 17:20:20 +0200

    commit

---
+++ MedinaCSharpLearing/Aufgabe2.cs
@@ -0,0 +1,25 @@
+﻿namespace MedinaCSharpLearing;
+
+public class Aufgabe2 {
+    public void test() {
+        //Arbeit mit STRINGS
+        // VERZWEIGUNGEN ->  IF ABFRAGEN
+
+        //Alles was text ist -> String = text
+
+        //ARRAY -> Feste Menge an Elementen
+
+
+        Console.WriteLine("Wie viel Uhr ist es?");
+        var anwort = Console.ReadLine().Split(" "); // 2 0
+        var stunde = anwort[0];
+        var minute = anwort[1];
+
+
+        var diesIstEinString = "Der Text wird geteilt!";
+
+        var befehl = diesIstEinString.Split(" ")[2];
+
+        Console.WriteLine(befehl);
+    }
+}
---
+++ MedinaCSharpLearing/Program.cs
@@ -0,0 +1,25 @@
+﻿//Erfahren was sagt uns der Nutzer,
+//Die Infos irgendwo speichern
+//Die Infos "stukturiert" ausgeben können -> print
+
+using MedinaCSharpLearing;
+
+new Aufgabe2().test();
+
+void NewFunction() {
+    Console.WriteLine("Wie heisst du?");
+
+    var wieHeißtDu = Console.ReadLine();
+
+    Console.WriteLine("Wo wohnst du?");
+
+    var woWohnstDu = Console.ReadLine();
+
+    Console.WriteLine("Wie alt bist du?");
+
+    var wieAltBistDu = Console.ReadLine();
+
+
+//Dein Name ist NAME, du bist ALTER Jahre alt und du wohnst in ADRESSE.
+    Console.WriteLine("Dein Name ist " + wieHeißtDu + ", du bist " + wieAltBistDu + " und du wohnst in " + woWohnstDu + ".");
+}
